bbdff1172fd31939ec43bf6c32199eec126b5484
yelim <yelim785@gmail.com>
2022-12-08 19:41:45 +0900

    아직 차트가 안나옴

--- tic_scraper/urls.py
+++ tic_scraper/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from alarm.views import index
+from alarm.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    # path('dashboard/', dashboard),
+    path('dashboard/', dashboard),
 ]
